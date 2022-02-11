d741a4369b8e73c5accba2cf078677146fafcfa3
Kera Cudmore <kera.cudmore@hotmail.com>
2022-02-11 17:46:40 +0000

    tidying up of file

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,7 +2,7 @@ function userInformationHTML(user) {
     return `
     <h2>${user.name}
     <span class="small-name">
-        (@<a href="${user.html_url}" target="_blank"> ${user.login}</a>
+        @<a href="${user.html_url}" target="_blank"> ${user.login}</a>
         </span>
         </h2>
         <div class="gh-content">
@@ -16,8 +16,8 @@ function userInformationHTML(user) {
 };
 
 function repoInformationHTML(repos) {
-    if (repos.length === 0) {
-        return `<div class="clearfix repo-list>No Repos!</div>`;
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No Repos!</div>`;
     }
 
     var listItemsHTML = repos.map(function(repo) {
@@ -28,7 +28,7 @@ function repoInformationHTML(repos) {
 
     return `<div class="clearfix repo-list>
                 <p>
-                    <stong>Repo List:</stong>
+                    <strong>Repo List:</strong>
                 </p>
                 <ul>
                     ${listItemsHTML.join("\n")}
@@ -55,12 +55,13 @@ function fetchGitHubInformation(event) {
             $.getJSON(`https://api.github.com/users/${username}/repos`)
         
         ).then(
-            function(firstResponse, secondresponse) {
+            function(firstResponse, secondResponse) {
                 var userData = firstResponse[0];
                 var repoData = secondResponse[0];
                 $("#gh-user-data").html(userInformationHTML(userData));
                 $("#gh-repo-data").html(repoInformationHTML(repoData));
-            }, function(errorResponse) {
+            }, 
+            function(errorResponse) {
                 if (errorResponse.status === 404) {
                     $("gh-user-data").html(
                         `<h2>No info found for user ${username}</h2>`);
@@ -72,8 +73,7 @@ function fetchGitHubInformation(event) {
                     $("#gh-user-data").html(
                         `<h2>Error: ${errorResponse.responseJSON.message}</h2>`)
                 }
-            }
-        )
+            });
 }
 
 $(document).ready(fetchGitHubInformation);
